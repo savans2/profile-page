8b7acec27eb9f5aa04d03dab64386e7c81b25494
Sava <sava.paroski021@gmail.com>
2020-09-07 18:58:36 +0200

    Added example content.

--- src/components/AboutSection.js
+++ src/components/AboutSection.js
@@ -1,8 +1,29 @@
-import React from 'react'
+import React, { useState } from 'react'
 
 export default function AboutSection() {
+  const [hover, setHover] = useState();
+
   return (
-    <div className="col-4 bg-dark rounded" style={{ height: '400px' }}>
+    <div className="col-4 p-3 mb-2 bg-dark rounded">
+      <p className="text-white">About</p>
+      <div className="col-12 my-3 bg-light d-flex align-items-center rounded" style={{ height: '30px' }}
+        onMouseEnter={() => { setHover('LivesIn') }} onMouseLeave={() => { setHover(undefined) }}>
+        <span className="mr-3" role="img" aria-label="house emoji">🏠</span>
+        <span className="mx-auto">Lives in Novi Sad</span>
+        {hover === 'LivesIn' ? <span>✏</span> : ''}
+      </div>
+      <div className="col-12 my-3 bg-light d-flex align-items-center rounded" style={{ height: '30px' }}
+        onMouseEnter={() => { setHover('WorksAt') }} onMouseLeave={() => { setHover(undefined) }}>
+        <span className="mr-3" role="img" aria-label="work building emoji">🏢</span>
+        <span className="mx-auto">Works at Company name </span>
+        {hover === 'WorksAt' ? <span>✏</span> : ''}
+      </div>
+      <div className="col-12 my-3 bg-light d-flex align-items-center rounded" style={{ height: '30px' }}
+        onMouseEnter={() => { setHover('Followers') }} onMouseLeave={() => { setHover(undefined) }}>
+        <span className="mr-3" role="img" aria-label="heart emoji ">🧡</span>
+        <span className="mx-auto">Followers 78</span>
+        {hover === 'Followers' ? <span role="img" aria-label="pen emoji">✏</span> : ''}
+      </div>
     </div>
   )
 }
