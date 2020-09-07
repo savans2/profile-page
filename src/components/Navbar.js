5c866924b35462f63bfd7b1f703ce704fca34ebb
Sava <sava.paroski021@gmail.com>
2020-09-07 18:01:56 +0200

    Added rounded.

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -2,7 +2,7 @@ import React from 'react'
 
 export default function Navbar() {
   return (
-    <nav class="navbar navbar-dark bg-dark">
+    <nav class="navbar navbar-dark bg-dark rounded-bottom">
       <a class="navbar-brand" href="#">Navbar</a>
     </nav>
   )
