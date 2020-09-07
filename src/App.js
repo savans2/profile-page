ec9796afb53885539fd51e7453d0999a631a3369
Sava <sava.paroski021@gmail.com>
2020-09-07 16:03:29 +0200

    Created basic layout.

--- src/App.js
+++ src/App.js
@@ -1,8 +1,18 @@
 import React from 'react';
+import Navbar from './components/Navbar';
+import ImageSection from './components/ImageSection';
+import AboutSection from './components/AboutSection';
 
 function App() {
   return (
     <div className="App">
+      <Navbar></Navbar>
+      <div className="container">
+        <ImageSection></ImageSection>
+        <div className="d-flex flex-wrap justify-content-between">
+          <AboutSection></AboutSection>
+        </div>
+      </div>
     </div>
   );
 }
