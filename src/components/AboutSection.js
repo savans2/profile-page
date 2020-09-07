ab74487161dd4e0c7e0f867d816ea5552dab0b2c
Sava <sava.paroski021@gmail.com>
2020-09-07 18:01:26 +0200

    Added rounded.

--- src/components/AboutSection.js
+++ src/components/AboutSection.js
@@ -2,7 +2,7 @@ import React from 'react'
 
 export default function AboutSection() {
   return (
-    <div className="col-4 bg-dark" style={{ height: '400px' }}>
+    <div className="col-4 bg-dark rounded" style={{ height: '400px' }}>
     </div>
   )
 }
