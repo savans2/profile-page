fbb84a1efd445d4b2b7bc23279e7ccb14b149a9d
Sava <sava.paroski021@gmail.com>
2020-09-07 22:26:07 +0200

    User is now able to edit where he lives/works.

--- src/components/AboutSection.js
+++ src/components/AboutSection.js
@@ -2,27 +2,46 @@ import React, { useState } from 'react'
 
 export default function AboutSection() {
   const [hover, setHover] = useState();
+  const [editMode, setEditMode] = useState(false);
 
   return (
     <div className="col-4 p-3 mb-2 bg-dark rounded">
       <p className="text-white">About</p>
-      <div className="col-12 my-3 bg-light d-flex align-items-center rounded" style={{ height: '30px' }}
-        onMouseEnter={() => { setHover('LivesIn') }} onMouseLeave={() => { setHover(undefined) }}>
-        <span className="mr-3" role="img" aria-label="house emoji">🏠</span>
-        <span className="mx-auto">Lives in Novi Sad</span>
-        {hover === 'LivesIn' ? <span>✏</span> : ''}
-      </div>
-      <div className="col-12 my-3 bg-light d-flex align-items-center rounded" style={{ height: '30px' }}
-        onMouseEnter={() => { setHover('WorksAt') }} onMouseLeave={() => { setHover(undefined) }}>
-        <span className="mr-3" role="img" aria-label="work building emoji">🏢</span>
-        <span className="mx-auto">Works at Company name </span>
-        {hover === 'WorksAt' ? <span>✏</span> : ''}
-      </div>
-      <div className="col-12 my-3 bg-light d-flex align-items-center rounded" style={{ height: '30px' }}
-        onMouseEnter={() => { setHover('Followers') }} onMouseLeave={() => { setHover(undefined) }}>
+      {
+        editMode === 'LivesIn' ?
+          <div className="col-12 my-3 bg-light d-flex align-items-center rounded" style={{ height: '30px' }}
+            onMouseEnter={() => { setHover('LivesIn') }} onMouseLeave={() => { setHover(undefined) }}>
+            <span className="mr-3" role="img" aria-label="house emoji">🏠</span>
+            <input className="bg-light mx-auto" type="text" style={{ border: '0px solid black' }} value="Lives in Novi Sad" autoFocus />
+            <span onClick={() => setEditMode(undefined)}>✔</span>
+          </div> :
+          <div className="col-12 my-3 bg-light d-flex align-items-center rounded" style={{ height: '30px' }}
+            onMouseEnter={() => { setHover('LivesIn') }} onMouseLeave={() => { setHover(undefined) }}>
+            <span className="mr-3" role="img" aria-label="house emoji">🏠</span>
+            <span className="mx-auto">Lives in Novi Sad</span>
+            {hover === 'LivesIn' ? <span onClick={() => { setEditMode('LivesIn') }}>✏</span> : <span className="invisible">✏</span>}
+          </div>
+      }
+
+      {
+        editMode === 'WorksAt' ?
+          <div className="col-12 my-3 bg-light d-flex align-items-center rounded" style={{ height: '30px' }}
+            onMouseEnter={() => { setHover('WorksAt') }} onMouseLeave={() => { setHover(undefined) }}>
+            <span className="mr-3" role="img" aria-label="house emoji">🏢</span>
+            <input className="bg-light mx-auto" type="text" style={{ border: '0px solid black' }} value="Works at Company name" autoFocus />
+            <span onClick={() => setEditMode(undefined)}>✔</span>
+          </div> :
+          <div className="col-12 my-3 bg-light d-flex align-items-center rounded" style={{ height: '30px' }}
+            onMouseEnter={() => { setHover('WorksAt') }} onMouseLeave={() => { setHover(undefined) }}>
+            <span className="mr-3" role="img" aria-label="work building emoji">🏢</span>
+            <span className="mx-auto">Works at Company name </span>
+            {hover === 'WorksAt' ? <span onClick={() => { setEditMode('WorksAt') }}>✏</span> : <span className="invisible">✏</span>}
+          </div>
+      }
+
+      <div className="col-12 my-3 bg-light d-flex align-items-center rounded" style={{ height: '30px' }}>
         <span className="mr-3" role="img" aria-label="heart emoji ">🧡</span>
         <span className="mx-auto">Followers 78</span>
-        {hover === 'Followers' ? <span role="img" aria-label="pen emoji">✏</span> : ''}
       </div>
     </div>
   )
